6e04e4ef93bf7487efa397c86f7606233f1e09ae
meghaarora2001 <72010555+meghaarora2001@users.noreply.github.com>
2022-03-15 20:49:30 +0530

    Add files via upload

---
+++ popout.js
@@ -0,0 +1,8 @@
+document.querySelector('button').addEventListener('click' , function(){
+    var SelectedTrack = document.querySelector('select').value;
+    chrome.runtime.sendMessage({name:"playTrack", track : SelectedTrack}) //sending message to our background.js
+    });
+    
+    document.querySelector('button.pause').addEventListener('click', function(){
+        chrome.runtime.sendMessage({name: "pauseTrack"});
+      });
